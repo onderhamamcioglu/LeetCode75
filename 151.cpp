4763f97372b9e9758b18d8923a5e65f44efbc051
Önder <eduhamoen001@fh-kaernten.ac.at>
2025-04-30 15:19:28 +0200

    151 solved

---
+++ 151.cpp
@@ -0,0 +1,70 @@
+/*
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words.
+The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words.
+The returned string should only have a single space separating the words. Do not include any extra spaces.
+ */
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string temp;
+        string res;
+        while (s[0] == ' ') {s.erase(s.begin());}
+        while (s[s.size()-1] == ' ') {s.pop_back();}
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                temp.insert(temp.begin(),s[i]);
+            }
+            else if (temp != ""){
+                res += temp + ' ';
+                temp.clear();
+            }
+        }
+        res += temp;
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << '\"' <<s.reverseWords("  hello world  ") << '\"';
+}
